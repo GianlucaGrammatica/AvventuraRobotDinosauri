28a8159f321e896396ce069aab339ae32231eed7
Gianluca <10934204@itisrossi.vi.it>
2024-11-15 09:39:10 +0100

    modifichine 1

---
+++ AvventuraRobotDinosauri/Componenti.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvventuraRobotDinosauri
+{
+    internal class Componenti
+    {
+        private string[] PossibleComponentys = {"Ferro", "Uranio", "Acciaio", "Cavi", "Lucine", "Interruttori", "Computer Qauuntistico", "Paperella decorativa"};
+        public string Componente { get;}
+        public int id;
+        Random random = new Random();
+
+        public Componenti() { 
+            int index = random.Next(PossibleComponentys.Length);
+            Componente = PossibleComponentys[index];
+            id = 0;
+        }
+
+        public Componenti(int _id)
+        {
+            int index = random.Next(PossibleComponentys.Length);
+            Componente = PossibleComponentys[index];
+            id = _id;
+        }
+    }
+}
---
+++ AvventuraRobotDinosauri/RobotDinosauri.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AvventuraRobotDinosauri
+{
+    internal class RobotDinosauri
+    {
+        private List<Task> DinosauriList = new List<Task>();
+        private List<Task> RobotList = new List<Task>();
+
+        private int CntComponents = 0;
+
+        private Stack<Componenti> ComponentsStack = new Stack<Componenti>();
+        private Queue<Componenti> ComponentsQueue = new Queue<Componenti>();
+
+        private readonly object Lock = new object();
+
+        private void Robot()
+        {
+            while(true)
+            {
+                lock (Lock)
+                {
+                    Componenti NewComponente = new Componenti();
+                    ComponentsQueue.Enqueue(NewComponente);
+                    Monitor.Pulse(Lock);
+                    Task.Delay(1000);
+                }
+            }
+            
+        }
+
+        private void Dinosauro()
+        {
+            while (true)
+            {
+                lock (Lock)
+                {
+                    if (ComponentsQueue.Count == 0)
+                    {
+                        Monitor.Wait(Lock);
+                    }
+
+                    Componenti TookComponente = ComponentsQueue.Dequeue();
+                    ComponentsStack.Push(TookComponente);
+                    Monitor.Pulse(Lock);
+                    Task.Delay(1000);
+                }
+            }
+
+        }
+    }
+}
